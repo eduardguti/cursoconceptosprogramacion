e71b91fcf9c969509ffac946654a7052622bfc77
Marco <A01746163@itesm.mx>
2023-08-09 12:59:44 -0600

    Update ejercicio de programacion en python15.py

--- curso de lenguage de programacion python/ejercicio de programacion en python15.py
+++ curso de lenguage de programacion python/ejercicio de programacion en python15.py
@@ -4,3 +4,19 @@ a = int(input("digite un numero : "))
 b = int(input("digite un numero : ")) 
 print(f"el numero es {a}") 
 print(f"el numero es {b}") 
+
+#a = 4
+#b = 6
+#c = 8
+
+# a = 8, b = 6, c = 4
+# c,a = a,c
+# a,b = b,a
+# b,c = c,b 
+
+#a,b,c = c,b,a 
+
+# a,b = b,a 
+
+#print(a)
+#print(b)
