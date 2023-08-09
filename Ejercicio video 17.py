882e43f537b051a3c950e754e3a4c26f7d2fca0c
Marco <A01746163@itesm.mx>
2023-08-09 13:59:41 -0600

    Avance video 17

---
+++ Ejercicio video 17.py
@@ -0,0 +1,6 @@
+compra_de_usuario = int(imput("ingreso de la compra"))
+descuento = .15 
+calculode_descuento = compra_de_usuario * .15 
+total_de_descuento = compra_de_usuario - .15 
+resultado_de_mi_compra(f"total de mi compra{calculo de descuento} {total de descuento}") 
+print(resutado_de_mi_compra)
