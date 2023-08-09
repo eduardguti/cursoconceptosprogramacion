b27d2b2c2cd6bb6a1f9314763534892c5e938c53
Marco <A01746163@itesm.mx>
2023-08-09 13:02:59 -0600

    Ejercicio 16

---
+++ ejercicio de programacion en python 16.py
@@ -0,0 +1,11 @@
+
+import math
+
+radio = float(input("mi radio es")) #imput es para ingresar 
+π = 3.1416
+area = π * radio**2  ##** elevar potencia
+longitud = 2* π * radio 
+print(area)
+print(longitud) 
+
+print(math.pi) #math.pi libreria para operaciones matematicas 
